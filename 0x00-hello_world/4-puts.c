1e82fa38119304cce0480218fb283dd255ee2fa5
Dmechar <dennismecha.dm@gmail.com>
2022-12-08 10:31:43 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,10 @@
  *
  * return: Always 0 (Success)
  * */
+
 int main(void){
-	puts("\"programming is like building a multilingual puzzle");
+	puts ("\"programming is like building a multilingual puzzle");
 	return(0);
+
+
 }
